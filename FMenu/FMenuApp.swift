8043bb5fbc9958b3cc258a472b99ce4239a4fbf0
Flavian Kaufmann <flavian@flaviankaufmann.ch>
2024-10-17 16:39:59 +0200

    Initial Commit

---
+++ FMenu/FMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMenuApp.swift
+//  FMenu
+//
+//  Created by Flavian Kaufmann on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
